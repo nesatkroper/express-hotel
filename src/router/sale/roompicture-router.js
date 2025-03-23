c53472e1b1e52e9ff80b41b742896d69a19635d8
megalobob <phanunsuon@gmail.com>
2025-03-23 21:36:58 +0700

    modified:   package.json 	new file:   public/uploads/picture-1742739389359-643265320.jpg 	new file:   public/uploads/picture-1742739619365-413740756.jpg 	new file:   public/uploads/picture-1742739906463-545815877.jpg 	new file:   public/uploads/room-picture/1742739257666.jpg 	new file:   public/uploads/room-picture/1742739257667.jpg 	modified:   src/router/sale/roompicture-router.js

--- src/router/sale/roompicture-router.js
+++ src/router/sale/roompicture-router.js
@@ -1,37 +1,7 @@
 const express = require("express");
-const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
+const { upload } = require("@/middleware/storage-middleware");
 
 const router = express.Router();
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const uploadDir = path.join(
-      __dirname,
-      "../../../public/uploads/room-picture"
-    );
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir, { recursive: true });
-    }
-    cb(null, "public/uploads/room-picture");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({
-  storage,
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png")
-      cb(null, true);
-    else cb(new Error("Invalid file type"), false);
-
-    cb(null, true);
-  },
-  limits: { fileSize: 10 * 1024 * 1024 },
-});
 
 const {
   select,

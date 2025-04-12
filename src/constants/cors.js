463e66c29bf41afc8c81b6834da13efb694d3dc7
megalobob <phanunsuon@gmail.com>
2025-04-12 21:51:49 +0700

    modified:   package.json 	modified:   prisma/schema.prisma 	modified:   src/constants/cors.js

--- src/constants/cors.js
+++ src/constants/cors.js
@@ -1,19 +1,15 @@
-const allowedOrigins = [
-    "https://react-hotel-two.vercel.app",
-    "https://nun.up.railway.app",
-    "http://localhost:5173",
-];
+const allowedOrigins = ["http://localhost:5173"];
 
 const allowedHeaders = [
-    "Origin",
-    "X-Requested-With",
-    "Content-Type",
-    "Accept",
-    "Authorization",
+  "Origin",
+  "X-Requested-With",
+  "Content-Type",
+  "Accept",
+  "Authorization",
 ];
 
 const methods = ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"];
 
 const transports = ["websocket", "polling"];
 
-module.exports = { allowedOrigins, allowedHeaders, methods, transports }
+module.exports = { allowedOrigins, allowedHeaders, methods, transports };
